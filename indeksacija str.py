1b71eeef74a3a9bc6271cd7a21d3d5ead87bf1f5
DenisKraw <Chiks1187@gmail.com>
2024-10-22 21:57:35 +0300

    Add files via upload

--- indeksacija str.py
+++ indeksacija str.py
@@ -1,6 +1,7 @@
 figura='параллелограмм'
+mid_index=len(figura)//2
 print(figura[0])
 print(figura[-1])
-print(figura[3:])
+print(figura[mid_index:])
 print(figura[::-1])
-print(figura[::2])
+print(figura[1::2])
